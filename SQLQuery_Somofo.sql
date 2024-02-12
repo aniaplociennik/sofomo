ed8eaea04eb9f9c65169b7e1e1698ecabc762aaa
aniaplociennik <62815989+aniaplociennik@users.noreply.github.com>
2024-02-12 22:06:31 +0100

    Add files via upload

--- SQLQuery_Somofo.sql
+++ SQLQuery_Somofo.sql
@@ -4,41 +4,41 @@
 
 WITH
 A_temp AS (
-    SELECT A.d1, M.d2_correct as d2, A.m1
-    FROM [somofo].[dbo].[A] A
-    INNER JOIN (SELECT DISTINCT d1, d2_correct FROM MAP) M ON A.d1 = M.d1
+    SELECT A.dimension_1, M.correct_dimension_2 as dimension_2, A.measure_1
+    FROM [A] A
+    INNER JOIN (SELECT DISTINCT dimension_1, correct_dimension_2 FROM MAP) M ON A.dimension_1 = M.dimension_1
 ),
 B_temp AS (
-    SELECT B.d1, M.d2_correct AS d2, B.m2
-    FROM [somofo].[dbo].[B] B
-    INNER JOIN (SELECT DISTINCT d1, d2_correct FROM MAP) M ON B.d1 = M.d1
+    SELECT B.dimension_1, M.correct_dimension_2 AS dimension_2, B.measure_2
+    FROM [B] B
+    INNER JOIN (SELECT DISTINCT dimension_1, correct_dimension_2 FROM MAP) M ON B.dimension_1 = M.dimension_1
 )
-SELECT COALESCE(A.d1, B.d1) AS d1,
-       COALESCE(A.d2, B.d2) AS d2,
-       ISNULL(SUM(A.m1), 0) AS sum_m1,
-       ISNULL(SUM(B.m2), 0) AS sum_m2
+SELECT COALESCE(A.dimension_1, B.dimension_1) AS dimension_1,
+       COALESCE(A.dimension_2, B.dimension_2) AS dimension_2,
+       ISNULL(SUM(A.measure_1), 0) AS measure_1,
+       ISNULL(SUM(B.measure_2), 0) AS measure_2
 FROM A_temp as A
-FULL OUTER JOIN B_temp as B ON A.d1 = B.d1 AND A.d2 = B.d2
-GROUP BY COALESCE(A.d1, B.d1), COALESCE(A.d2, B.d2);
+FULL OUTER JOIN B_temp as B ON A.dimension_1 = B.dimension_1 AND A.dimension_2 = B.dimension_2
+GROUP BY COALESCE(A.dimension_1, B.dimension_1), COALESCE(A.dimension_2, B.dimension_2);
 
 /*
 WITH
 A_temp AS (
-    SELECT A.d1, M.d2_correct as d2, sum(A.m1) as m1
-    FROM [somofo].[dbo].[A] A
-    INNER JOIN (SELECT DISTINCT d1, d2_correct FROM MAP) M ON A.d1 = M.d1
-GROUP BY A.d1, M.d2_correct
+    SELECT A.dimension_1, M.correct_dimension_2 as dimension_2, sum(A.measure_1) as measure_1
+    FROM [A] A
+    INNER JOIN (SELECT DISTINCT dimension_1, correct_dimension_2 FROM MAP) M ON A.dimension_1 = M.dimension_1
+GROUP BY A.dimension_1, M.correct_dimension_2
 ),
 B_temp AS (
-    SELECT B.d1, M.d2_correct AS d2, B.m2
-    FROM [somofo].[dbo].[B] B
-    INNER JOIN (SELECT DISTINCT d1, d2_correct FROM MAP) M ON B.d1 = M.d1
+    SELECT B.dimension_1, M.correct_dimension_2 AS dimension_2, B.measure_2
+    FROM [B] B
+    INNER JOIN (SELECT DISTINCT dimension_1, correct_dimension_2 FROM MAP) M ON B.dimension_1 = M.dimension_1
 )
-SELECT COALESCE(A.d1, B.d1) AS d1,
-       COALESCE(A.d2, B.d2) AS d2,
-       ISNULL(SUM(A.m1), 0) AS sum_m1,
-       ISNULL(SUM(B.m2), 0) AS sum_m2
+SELECT COALESCE(A.dimension_1, B.dimension_1) AS dimension_1,
+       COALESCE(A.dimension_2, B.dimension_2) AS dimension_2,
+       ISNULL(SUM(A.measure_1), 0) AS measure_1,
+       ISNULL(SUM(B.measure_2), 0) AS measure_2
 FROM A_temp as A
-FULL OUTER JOIN B_temp as B ON A.d1 = B.d1 AND A.d2 = B.d2
-GROUP BY COALESCE(A.d1, B.d1), COALESCE(A.d2, B.d2);
+FULL OUTER JOIN B_temp as B ON A.dimension_1 = B.dimension_1 AND A.dimension_2 = B.dimension_2
+GROUP BY COALESCE(A.dimension_1, B.dimension_1), COALESCE(A.dimension_2, B.dimension_2);
 */
